104505a096ca6521b1bee5622b643ade899e1955
kas0070 <59895196+kas0070@users.noreply.github.com>
2020-01-16 13:07:49 -0500

    Katie Stanley task 01 upload b

---
+++ task_01/task01.r
@@ -0,0 +1,94 @@
+libary (swirl)
+swirl ()
+SunshineKatie
+
+2
+
+1
+1
+1
+1
+1
+
+
+5+7
+
+
+
+
+x<- 5+7
+
+x
+y< x-3
+x-3
+y<- x-3
+y
+
+c(1.1, 9, 3.14)
+z<- c(1.1, 9, 3.14)
+?c
+z
+z 555
+z, 555 z
+c(z, 555,z)
+z*2 + 100
+
+
+
+my_sqrt<- sqrt(z-1)
+1
+my_sqrt
+
+my_div<- (z/my_sqrt)
+my_div <- z/ my_sqrt
+3
+my_div
+
+
+
+c(1,2,3,4)+c(0,10)
+
+c(1,2,3,4)+c(0,10,100)
+
+z*2 +1000
+
+my_div
+1
+kas0070@mix.wvu.edu
+TASK01B
+1
+1
+0
+2
+SWIRL()
+swirl()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
